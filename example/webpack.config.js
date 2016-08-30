1cb0414c342d68a3688650a0689753e8f507fd11
Paul Myburgh <paulmyburgh88@gmail.com>
2016-08-30 13:24:54 +0200

    testing images

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, '../gh-pages'),
     filename: 'bundle.js',
-    publicPath: '/__build__/',
+    publicPath: '/react-validate/gh-pages/',
   },
 
   module: {
